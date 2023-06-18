e831eb3113834e9b24c84e8078176d417f8c7d85
Sangam Biradar <sangam14@users.noreply.github.com>
2023-06-18 17:20:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://kubedaily.com/cloudnativetools',
+  base: 'https://wasmjam.github.io/wasmtools',
   plugins: [react()]
 })
