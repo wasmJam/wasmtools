deb9774b230d7d55d881baabb156ceeb9ff2f127
Sangam Biradar <sangam14@users.noreply.github.com>
2023-12-22 03:35:44 +0530

    Update entries.js

--- src/data/entries.js
+++ src/data/entries.js
@@ -15,20 +15,40 @@
 
 const entries = [
   {
-    title: "argocd",
-    link: "",
-    description: "Declarative continuous deployment for Kubernetes",
-    tag: "Continuous Integration & Delivery",
-    github: "argoproj/argo-cd",
+    "title": "emscripten-core",
+    "link": NaN,
+    "description": "Emscripten: An LLVM-to-WebAssembly Compiler",
+    "tag": "compiler",
+    "github": "emscripten-core/emscripten"
   },
-
   {
-    title: "agola",
-    link: "",
-    description: "Agola: CI/CD Redefined ",
-    tag: "Continuous Integration & Delivery",
-    github: "agola-io/agola",
+    "title": "binaryen",
+    "link": NaN,
+    "description": "Optimizer and compiler/toolchain library for WebAssembly",
+    "tag": "compiler",
+    "github": "WebAssembly/binaryen"
   },
+  {
+    "title": "tinygo",
+    "link": NaN,
+    "description": "Go compiler for small places. Microcontrollers, WebAssembly (WASM/WASI), and command-line tools. Based on LLVM.",
+    "tag": "compiler",
+    "github": "tinygo-org/tinygo"
+  },
+  {
+    "title": "Bytecoder",
+    "link": NaN,
+    "description": "Framework to interpret and transpile JVM bytecode to JavaScript, OpenCL or WebAssembly.",
+    "tag": "compiler",
+    "github": "mirkosertic/Bytecoder"
+  },
+  {
+    "title": "assemblyscript",
+    "link": NaN,
+    "description": "A TypeScript-like language for WebAssembly",
+    "tag": "compiler",
+    "github": "AssemblyScript/assemblyscript"
+  }
 ];
 
 export default entries;
