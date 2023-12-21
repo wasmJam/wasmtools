b2a2e18bee20d2a321278360ba58f53f58670931
sangam14 <smbiradar14@gmail.com>
2023-12-22 04:27:07 +0530

    Your commit message

--- src/data/entries.js
+++ src/data/entries.js
@@ -15,6 +15,7 @@
 
 const entries = [
   {
+<<<<<<< HEAD
     "title": "emscripten-core",
     "link": NaN,
     "description": "Emscripten: An LLVM-to-WebAssembly Compiler",
@@ -25,10 +26,53 @@ const entries = [
     "title": "binaryen",
     "link": NaN,
     "description": "Optimizer and compiler/toolchain library for WebAssembly",
+=======
+    "title": "assemblyscript",
+    "link": NaN,
+    "description": "A TypeScript-like language for WebAssembly",
+    "tag": "compiler",
+    "github": "AssemblyScript/assemblyscript"
+  },
+  {
+    "title": "Bytecoder",
+    "link": NaN,
+    "description": "Framework to interpret and transpile JVM bytecode to JavaScript, OpenCL or WebAssembly.",
+>>>>>>> 3221912 (add)
     "tag": "compiler",
     "github": "WebAssembly/binaryen"
   },
   {
+<<<<<<< HEAD
+=======
+    "title": "binaryen",
+    "link": NaN,
+    "description": "Optimizer and compiler/toolchain library for WebAssembly",
+    "tag": "compiler",
+    "github": "WebAssembly/binaryen"
+  },
+  {
+    "title": "cheerp-compiler",
+    "link": NaN,
+    "description": "C++ to JavaScript/WebAssembly compiler",
+    "tag": "compiler",
+    "github": "leaningtech/cheerp-compiler"
+  },
+  {
+    "title": "emscripten-core",
+    "link": NaN,
+    "description": "Emscripten: An LLVM-to-WebAssembly Compiler",
+    "tag": "compiler",
+    "github": "emscripten-core/emscripten"
+  },
+  {
+    "title": "JWebAssembly",
+    "link": NaN,
+    "description": "Java bytecode to WebAssembly compiler",
+    "tag": "compiler",
+    "github": "i-net-software/JWebAssembly"
+  },
+  {
+>>>>>>> 3221912 (add)
     "title": "tinygo",
     "link": NaN,
     "description": "Go compiler for small places. Microcontrollers, WebAssembly (WASM/WASI), and command-line tools. Based on LLVM.",
@@ -36,6 +80,7 @@ const entries = [
     "github": "tinygo-org/tinygo"
   },
   {
+<<<<<<< HEAD
     "title": "Bytecoder",
     "link": NaN,
     "description": "Framework to interpret and transpile JVM bytecode to JavaScript, OpenCL or WebAssembly.",
@@ -48,7 +93,15 @@ const entries = [
     "description": "A TypeScript-like language for WebAssembly",
     "tag": "compiler",
     "github": "AssemblyScript/assemblyscript"
+=======
+    "title": "waforth",
+    "link": NaN,
+    "description": "Small but complete dynamic Forth Interpreter/Compiler for and in WebAssembly",
+    "tag": "compiler",
+    "github": "remko/waforth"
+>>>>>>> 3221912 (add)
   }
 ];
+
 
 export default entries;
